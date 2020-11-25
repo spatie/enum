a31166a70bfcbb3b038f42dad2a22eb123cc36af
Tom Witkowski <dev.gummibeer@gmail.com>
2020-11-25 17:00:24 +0100

    add two new custom phpunit assertions

---
+++ tests/Phpunit/AssertIsEnumLabelTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Spatie\Enum\Tests\Phpunit;
+
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use Spatie\Enum\Phpunit\EnumAssertions;
+use Spatie\Enum\Tests\EnumWithLabels;
+use Spatie\Enum\Tests\MyEnum;
+
+class AssertIsEnumLabelTest extends TestCase
+{
+    /** @test */
+    public function it_passes(): void
+    {
+        EnumAssertions::assertIsEnumLabel(EnumWithLabels::class, 'a');
+    }
+
+    /** @test */
+    public function it_fails(): void
+    {
+        $this->expectException(ExpectationFailedException::class);
+
+        EnumAssertions::assertIsEnumLabel(EnumWithLabels::class, 'A');
+    }
+}
