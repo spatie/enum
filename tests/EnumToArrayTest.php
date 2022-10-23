c597cc50970714235bc134b2b776927080d2e64b
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2022-10-23 10:13:16 +0100

    refactoring tests in EnumToArrayTest

--- tests/EnumToArrayTest.php
+++ tests/EnumToArrayTest.php
@@ -2,58 +2,48 @@
 
 namespace Spatie\Enum\Tests;
 
-use PHPUnit\Framework\TestCase;
 use Spatie\Enum\Enum;
 
-class EnumToArrayTest extends TestCase
-{
-    /** @test */
-    public function test_to_array()
-    {
-        $this->assertSame(
-            ['A' => 'A', 'B' => 'B'],
-            SimpleEnumAsArray::toArray()
-        );
+test('to array', function () {
+    $this->assertSame(
+        ['A' => 'A', 'B' => 'B'],
+        SimpleEnumAsArray::toArray()
+    );
 
-        $this->assertSame(
-            ['A', 'B'],
-            SimpleEnumAsArray::toValues()
-        );
+    $this->assertSame(
+        ['A', 'B'],
+        SimpleEnumAsArray::toValues()
+    );
 
-        $this->assertSame(
-            ['A', 'B'],
-            SimpleEnumAsArray::toLabels()
-        );
-    }
+    $this->assertSame(
+        ['A', 'B'],
+        SimpleEnumAsArray::toLabels()
+    );
+});
 
-    /** @test */
-    public function test_to_array_with_value_and_label_map()
-    {
-        $this->assertSame(
-            ['A' => 'a', 2 => 'B'],
-            EnumAsArray::toArray()
-        );
+test('to array with value and label map', function () {
+    $this->assertSame(
+        ['A' => 'a', 2 => 'B'],
+        EnumAsArray::toArray()
+    );
 
-        $this->assertSame(
-            ['A', 2],
-            EnumAsArray::toValues()
-        );
+    $this->assertSame(
+        ['A', 2],
+        EnumAsArray::toValues()
+    );
 
-        $this->assertSame(
-            ['a', 'B'],
-            EnumAsArray::toLabels()
-        );
-    }
+    $this->assertSame(
+        ['a', 'B'],
+        EnumAsArray::toLabels()
+    );
+});
 
-    /** @test */
-    public function test_cases(): void
-    {
-        $all = SimpleEnumAsArray::cases();
+test('cases', function () {
+    $all = SimpleEnumAsArray::cases();
 
-        $this->assertTrue(SimpleEnumAsArray::A()->equals($all[0]));
-        $this->assertTrue(SimpleEnumAsArray::B()->equals($all[1]));
-    }
-}
+    $this->assertTrue(SimpleEnumAsArray::A()->equals($all[0]));
+    $this->assertTrue(SimpleEnumAsArray::B()->equals($all[1]));
+});
 
 /**
  * @method static self A()
